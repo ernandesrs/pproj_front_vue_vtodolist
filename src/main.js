d457a2c49f5d1f1b70cb24ec0cf3f04b96e97dcc
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-28 11:04:57 -0400

    Add vuex package

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import '@/assets/css/tailwind.css'
 import router from './router'
+import store from './store'
+
+import '@/assets/css/tailwind.css'
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount('#app')
