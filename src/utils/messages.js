cf4ff552c78b80a4e653435cc46f897d8e338d2a
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-08-30 17:55:45 -0400

    message

--- src/utils/messages.js
+++ src/utils/messages.js
@@ -1,4 +1,5 @@
 export default {
     ServerError: "Ops! Algo deu errado no servidor :(!",
     LoginInvalidException: "Ops! E-mail e/ou senha inválidos.",
+    NotFoundException: "Oops, parece que o que você busca não foi encontrado!",
 };
