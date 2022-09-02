4129014214d07ac691734e0d54528df80e7b32f6
Ernandes R de Souza <70029077+ernandesrs@users.noreply.github.com>
2022-09-02 08:17:07 -0400

    Correção de rota

--- src/components/auth/LoginMenu.vue
+++ src/components/auth/LoginMenu.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="text-center">
         <router-link class="px-2" to="/login">Login</router-link>
-        <router-link class="px-4" to="/register">Register</router-link>
+        <router-link class="px-4" to="/registrar">Register</router-link>
     </nav>
 </template>
 
